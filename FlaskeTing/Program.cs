d20356a36d07754ebd2662f99d9e5990c28e8b8d
GET Academy <terje@kolderup.net>
2020-03-31 14:39:30 +0200

    Initial commit

---
+++ FlaskeTing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FlaskeTing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simulation = new Simulation(1, 5, 7);
+            simulation.Run();
+        }
+    }
+}
---
+++ FlaskeTing/Simulation.cs
@@ -0,0 +1,37 @@
+﻿namespace FlaskeTing
+{
+    public class Simulation
+    {
+        public int WantedVolume { get; }
+        public Bottle Bottle1 { get; }
+        public Bottle Bottle2 { get; }
+
+        public bool IsSolved => Bottle1.Content + Bottle2.Content == WantedVolume
+                             || Bottle1.Content == WantedVolume 
+                             || Bottle2.Content == WantedVolume;
+
+        public Simulation(int wantedVolume, int volumeBottle1, int volumeBottle2)
+        {
+            WantedVolume = wantedVolume;
+            Bottle1 = new Bottle(volumeBottle1);
+            Bottle2 = new Bottle(volumeBottle2);
+        }
+
+
+        public void Run()
+        {
+            var numberOfOperations = 1;
+            while (true)
+            {
+                var operationSet = new OperationSet(numberOfOperations, this);
+                operationSet.RunAll();
+                if (IsSolved)
+                {
+                    operationSet.Show();
+                    break;
+                }
+                numberOfOperations++;
+            }
+        }
+    }
+}
