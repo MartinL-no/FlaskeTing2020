7702efefebb0cc22caeedf77dfaba66bb0a67859
MartinL-no <martin.lorimer@icloud.com>
2023-04-06 15:50:09 +0200

    create test project

---
+++ FlaskeTing.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FlaskeTing.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
