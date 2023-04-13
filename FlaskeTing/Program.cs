2e882c84c6bc8ed7e0bdec47ecfa3e6ff1deda48
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-04-13 20:43:54 +0200

    completed exercise

--- FlaskeTing/Program.cs
+++ FlaskeTing/Program.cs
@@ -6,9 +6,7 @@ namespace FlaskeTing
     {
         static void Main(string[] args)
         {
-            //var simulation = new Simulation(1, 5, 7);
-            //var simulation = new Simulation(3, 9, 4);
-            var simulation = new Simulation(4, 6, 8);
+            var simulation = new Simulation(1, 5, 7);
             var operationSet = simulation.Run();
             Console.WriteLine(operationSet.GetDescription());
         }
